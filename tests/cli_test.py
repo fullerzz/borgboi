97aa1f1aef4c545d73bb99272e5abe0e5c0af46d
Zach Fuller <zach.fuller1222@gmail.com>
2025-02-16 12:51:28 -0700

    test(delete-archive): add test for deleting archive

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -7,6 +7,7 @@ from click.testing import CliRunner
 from borgboi import orchestrator
 from borgboi.backups import BorgRepo
 from borgboi.cli import cli
+from tests.orchestrator_test import EXCLUDES_SRC
 
 
 def test_help() -> None:
@@ -34,11 +35,29 @@ def test_create_repo(
     assert "Created new Borg repo at" in result.stdout
 
 
-def test_delete_repo(
-    monkeypatch: pytest.MonkeyPatch, repo_storage_dir: Path, backup_target_dir: Path, borg_repo: BorgRepo
-) -> None:
+def test_delete_repo(repo_storage_dir: Path, borg_repo: BorgRepo) -> None:
     runner = CliRunner()
     result = runner.invoke(cli, ["delete-repo", "--repo-path", str(repo_storage_dir)], input=borg_repo.name)
     assert result.exit_code == 0
     assert "Repository deleted successfully" in result.stdout
     assert "Deleted repo from DynamoDB table" in result.stdout
+
+
+def test_delete_archive(repo_storage_dir: Path, borg_repo: BorgRepo) -> None:
+    from borgboi.orchestrator import create_excludes_list
+
+    # Create an exclusion list for the repo
+    new_excludes_file = create_excludes_list(borg_repo.name, EXCLUDES_SRC)
+    # Create an archive to be deleted in this test
+    archive_name = borg_repo.create_archive()
+    new_excludes_file.unlink()
+
+    runner = CliRunner()
+    deletion_confirm_input = f"{borg_repo.name}::{archive_name}"
+    result = runner.invoke(
+        cli,
+        ["delete-archive", "--repo-path", str(repo_storage_dir), "--archive-name", archive_name],
+        input=deletion_confirm_input,
+    )
+    assert result.exit_code == 0
+    assert "Archive deleted successfully" in result.stdout
